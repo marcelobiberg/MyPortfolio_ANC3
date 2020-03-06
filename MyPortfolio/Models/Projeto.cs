a022791d6f18d0b81e7fe87b37376fb68b4deb48
Marcelobiberg <biberg.marcelo@mail.com>
2020-03-06 16:07:36 -0300

    Adicionado layout para listar os projetos

---
+++ MyPortfolio/Models/Projeto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyPortfolio.Models
+{
+    public class Projeto
+    {
+        public string ID { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public string ImgBackground { get; set; }
+        public string Img01 { get; set; }
+        public string Img02 { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
