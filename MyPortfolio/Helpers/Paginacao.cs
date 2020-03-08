c5fee3e36d251d801ae8221ea14a634f9a4f8dca
Marcelobiberg <biberg.marcelo@mail.com>
2020-03-08 15:58:56 -0300

    Adicionado módulo de paginação

---
+++ MyPortfolio/Helpers/Paginacao.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Helpers
+{
+    /// <summary>Método responsável pala paginação
+    /// <para>Pagination</para>
+    /// <seealso cref=""/>
+    /// </summary>
+    public class Paginacao<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public Paginacao(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        public static async Task<Paginacao<T>> Pagination(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new Paginacao<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
